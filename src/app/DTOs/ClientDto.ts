625861152f9ff41f34c43ac8414a71a3c833e7c6
JayshreeeRam <mm8346959@gmail.com>
2025-10-03 15:53:59 +0530

    Completed

--- src/app/DTOs/ClientDto.ts
+++ src/app/DTOs/ClientDto.ts
@@ -1,18 +1,14 @@
 ﻿import { AccountStatus } from "../Enum/AccountStatus 1";
 import { AccountType } from "../Enum/AccountType 1";
 
-
 export interface ClientDto {
   clientId: number;
   name: string;
   address?: string | null;
-
   bankId: number;
   userId: number;
-
   accountNo: string;
-
-  verificationStatus?: AccountStatus ; 
-  accountType?: AccountType ;
+  verificationStatus?: AccountStatus; 
+  accountType?: AccountType;
+  rejectionRemark?: string; // Add this property for rejection remarks
 }
-
